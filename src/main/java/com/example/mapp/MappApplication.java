2d96a59694f95ff8637dee8879158c0b363001ee
flash548 <christopher.zell@gmail.com>
2024-08-05 22:23:50 -0400

    import cleanup

--- src/main/java/com/example/mapp/MappApplication.java
+++ src/main/java/com/example/mapp/MappApplication.java
@@ -1,22 +1,12 @@
 package com.example.mapp;
 
-import com.example.mapp.model.Program;
-import com.example.mapp.model.Role;
-import com.example.mapp.repository.FormRepository;
-import com.example.mapp.repository.ProgramRepository;
-import com.example.mapp.repository.RoleRepository;
-import com.example.mapp.service.RoleMappingService;
-import jakarta.transaction.Transactional;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 @SpringBootApplication
 public class MappApplication {
